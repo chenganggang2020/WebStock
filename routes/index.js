7a8d2561e8d02d9a8687efb57c58b2e48d96706f
chenganggang2020 <2568093252@qq.com>
2026-06-17 08:50:14 +0800

    feat: mount industry router

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ const stocksRouter = require('./stocks');
 const marketRouter = require('./market');
 const analysisRouter = require('./analysis');
 const portfolioRouter = require('./portfolio');
+const industryRouter = require('./industry');
 
 router.use('/api', stocksRouter);
 router.use('/api', marketRouter);
 router.use('/api', analysisRouter);
 router.use('/api/portfolio', portfolioRouter);
+// 行业相关接口挂载在 /api/industry
+router.use('/api/industry', industryRouter);
 
 module.exports = router;
